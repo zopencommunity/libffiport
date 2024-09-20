b6226738a1751d00f7da71b4529927c0d01ec685
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:56 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libffiport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libffiport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libffiport.git'), string(name: 'PORT_DESCRIPTION', value: 'test' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libffiport.git'), string(name: 'PORT_DESCRIPTION', value: 'test' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
